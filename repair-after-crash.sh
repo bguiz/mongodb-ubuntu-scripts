2be6c1fd308d06e26a2989cf2368b009e52c4736
Brendan Graetz <github@bguiz.com>
2015-11-09 13:15:03 +0800

    =BG= update to `killall` before doing anything else

--- repair-after-crash.sh
+++ repair-after-crash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Killing all mongo processes"
+killall -9 mongo mongod
 echo "Removing socket file"
 sudo rm /tmp/mongodb-27017.sock
 echo "Removing lock file"
@@ -7,4 +9,4 @@ sudo rm /var/lib/mongodb/mongod.lock
 echo "Running repair task"
 sudo -u mongodb mongod --repair
 echo "Starting mongo daemon"
-sudo service mongo start
+sudo service mongod start
